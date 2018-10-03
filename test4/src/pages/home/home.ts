b3f6cf382ae2152d8d147362c026061c372922ea
woraput <woraput@kkumail.com>
2018-10-03 17:42:49 +0700

    first commit

---
+++ test4/src/pages/home/home.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ModalController } from 'ionic-angular';
+import { ModalPage } from '../modal/modal';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
+
+  }
+
+ modalshow(){
+
+  const modal = this.modalCtrl.create(ModalPage);
+    modal.present();
+ }
+}
